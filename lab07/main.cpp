ec12952cf60a793c3571881dec78ec2403292783
dimitardimitrov2000 <125960950+dimitardimitrov2000@users.noreply.github.com>
2023-04-03 09:22:40 +0000

    CPP main file for Invoice program

--- lab07/main.cpp
+++ lab07/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include "Invoice.h"
+#include "Product.h"
+#include "Item.h"
 
 int main()
 {
-    
+    Product apple("apple", 2.90);
+    Product banana("banana", 1.65);
+
+    Item item1(apple, 2);
+    Item item2(banana, 3);
+
+    Invoice invoice;
+    invoice.addItem(item1);
+    invoice.addItem(item2);
+
+    std::cout << "Total: " << invoice.getTotal() << std::endl;
+
+    return 0; 
+
 }
